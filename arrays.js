6723de071b05c86a8cbfebfc6ed62361020f6cb4
Fatima <fatima.hammouri@gmail.com>
2021-03-20 13:25:42 +0000

    Declared printIndices Function

---
+++ arrays.js
@@ -0,0 +1,18 @@
+"use strict";
+
+
+// 1. printIndices
+function printIndices(items) {
+  for(let i = 0; i < items.length; i++){
+    console.log(`${items[i]} ${i}`);
+  }
+}
+printIndices(['apple', 'berry', 'banana'])
+
+// 2. everyOtherItem
+function everyOtherItem(items) {
+}
+
+// 3. smallestNItems
+function smallestNItems(items, n) {
+}
