9d7c6992ecfc7eb5497d5a9d5508ca288a0cf343
Fatima <fatima.hammouri@gmail.com>
2021-03-20 14:07:15 +0000

    :white_check_mark: Declared calculateTotal Function

--- functions.js
+++ functions.js
@@ -6,14 +6,34 @@
 function isHometown(town){
     return town === 'San Francisco'
 }
-const isHometown2 = (town) => town === 'san Francisco';
-
+const isHometown2 = (town) => town === 'San Francisco';
+    console.log(isHometown('Athens'));
 
 // 2. getFullName
 
-// Define your function here
-
+const getFullName = (firstName, lastName) => `${firstName} ${lastName}` 
+    console.log(getFullName('fatima', 'Alhammouri'));
 
 // 3. calculateTotal
+function calculateTotal (basePrice, state, tax=0.05){
+    let subtotal = basePrice * (1 + tax);
+    let fee = 0;
 
-// Define your function here
+    if (state === 'CA'){
+        fee = 0.03 * subtotal;
+    }
+    else if (state === 'PA'){
+        fee = 2;
+    }
+    else if (state === 'MA'){
+        if (basePrice <= 100) {
+            fee = 1;
+        }
+        else {
+            fee = 3;
+        }
+    }
+    return subtotal + fee;
+}
+console.log(calculateTotal(100, 'CA', 0.01))
+ 
