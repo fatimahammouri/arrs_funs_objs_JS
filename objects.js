b30176b80032b0e2509206b9a3136bffe2ae8a6a
Fatima <fatima.hammouri@gmail.com>
2021-03-20 14:41:05 +0000

    :construction_worker: Defined dict_{price :[melons]} & Return [melons]that cost (price)

--- objects.js
+++ objects.js
@@ -2,6 +2,17 @@
 
 
 // 1. countWords
+// Return a dictionary of each word and the no. of times they appear.
+
+// You can assume that `phrase` does not contain any punctuation and that
+//  each word is separated with a space.
+
+// Arguments:
+//   phrase (str): A string of space-separated words
+
+// Returns:
+//  dict: {word: # of times word appears in `phrase`}
+   
 function countWords(phrase) {
   let wordsArray = phrase.split(' ');
     console.log(wordsArray);
@@ -21,6 +32,26 @@ console.log(countWords('I like to code and I ike code'))
 
 
 // 2. getMelonsAtPrice
+/*
+Return the list of melons that cost `price`.
+Arguments:
+    price (float)
+Returns:
+    list: List of melons that cost `price`
+    None: If there are no melons that cost `price`
+*/ 
+
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  let melon_prices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  };
+  if(melon_prices[price] !== undefined){
+    return melon_prices[price].sort()
+  }
 }
+console.log(getMelonsAtPrice(2.50))
+console.log(getMelonsAtPrice(5))
+
