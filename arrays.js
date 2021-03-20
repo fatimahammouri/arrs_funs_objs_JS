35015753e0c815e34c9ea8da1a5c1f8fdde7c852
Fatima <fatima.hammouri@gmail.com>
2021-03-20 13:32:55 +0000

    :white_check_mark: Declared everyOtherItem Function

--- arrays.js
+++ arrays.js
@@ -11,7 +11,15 @@ printIndices(['apple', 'berry', 'banana'])
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
+  let result = []
+  for (let i = 0; i < items.length; i += 2){
+    result.push(items[i])
+  }
+
+  return result
 }
+console.log(everyOtherItem(['apple', 'berry', 'banana', 'melon', 'cherry']))
+
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
