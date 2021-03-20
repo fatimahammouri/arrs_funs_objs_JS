f3bcd26ab3fad67744ea3ad2a1b2233c06cfe7a7
Fatima <fatima.hammouri@gmail.com>
2021-03-20 14:56:31 +0000

    :construction_worker: Defined function => [unique words that appear in words1 & words2]

--- furtherStudy.js
+++ furtherStudy.js
@@ -2,9 +2,18 @@
 
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  let wordsSet1 = new Set(words1);
+  let wordsSet2 = new Set(words2);
+  let result = new Set();
+  for(const word of wordsSet1){
+    if(wordsSet2.has(word)){
+      result.add(word)
+    }
+  }
+  return [result]
+  
 }
-
+console.log(wordsInCommon(['a', 'b', 'c'],['a', 'b', 'b', 'd'] ))
 
 function kidsGame(names) {
   // Replace this with your code
